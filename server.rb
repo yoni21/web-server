96e4a7694d5a42803197c3e438767bd7b23e533a
yonas samuel <yonastlolo@yahoo.com>
2017-01-24 17:01:56 -0500

    server

--- server.rb
+++ server.rb
@@ -13,8 +13,37 @@ loop do                                             # Server runs forever
   while (line = client.gets) && !line.chomp.empty?  # Read the request and collect it until it's empty
     lines << line.chomp
   end
-  puts lines                                        # Output the full request to stdout
+  puts lines
 
-  client.puts(Time.now.ctime)                       # Output the current time to the client
+                                         # Output the full request to stdout
+
+#3
+  filename = lines[0].gsub(/GET \//, '').gsub(/\ HTTP.*/, '')
+
+  if File.exists?(filename)
+  response_body = File.read(filename)
+  success_header = []
+  success_header << "HTTP/1.1 200 OK"
+  success_header << "Content-Type: text/html" # should reflect the appropriate content type (HTML, CSS, text, etc)
+  success_header << "Content-Length: #{response_body.length}" # should be the actual size of the response body
+  success_header << "Connection: close"
+  header = success_header.join("\r\n")
+
+  response = [success_header, response_body].join("\r\n\r\n")
+else
+  response_body = "File Not Found\n" # need to indicate end of the string with \n
+  not_found_header = []
+  not_found_header << "HTTP/1.1 200 OK"
+  not_found_header << "Content-Type: text/html" # should reflect the appropriate content type (HTML, CSS, text, etc)
+  not_found_header << "Content-Length: #{response_body.length}" # should be the actual size of the response body
+  not_found_header << "Connection: close"
+  header = not_found_header.join("\r\n")
+  #5
+  response = [not_found_header, response_body].join("\r\n\r\n")
+
+end
+
+
+  client.puts(response)                       # Output the current time to the client
   client.close                                      # Disconnect from the client
 end
